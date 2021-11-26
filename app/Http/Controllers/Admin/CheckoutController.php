7d2e6f3eeeeeee65bf2432bfdc3675e349da51f5
suryaaji123 <81842762+suryaaji123@users.noreply.github.com>
2021-11-26 12:22:24 +0700

    integrate email for admin to paid

--- app/Http/Controllers/Admin/CheckoutController.php
+++ app/Http/Controllers/Admin/CheckoutController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Checkout;
+use Mail;
+use App\Mail\Checkout\paid;
 
 class CheckoutController extends Controller
 {
@@ -12,6 +14,10 @@ class CheckoutController extends Controller
     {
         $checkout->is_paid = true;
         $checkout->save();
+
+        // sending to user mail
+        Mail::to($checkout->User->email)->send(new Paid($checkout));
+        
         $request->session()->flash('success', "Checkout with id {$checkout->id} has been update!");
         return redirect(route('admin.dashboard'));
     }
